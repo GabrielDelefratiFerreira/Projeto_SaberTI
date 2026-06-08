6fc5d9032d7d10e3672ff3d8f1141133d1863be5
Gabriel Delefrati <168195936+GabrielDelefratiFerreira@users.noreply.github.com>
2026-06-08 10:03:25 -0400

    Primeiro commit

---
+++ clientes.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://bbphzdnivlifviemxgff.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_5DZjx6V6Cp68QRaFp-xE3g_u_lGR-CC";
+
---
+++ login.js
@@ -0,0 +1,28 @@
+const usuario = "admin";
+const senha = "123";
+
+const inputDoUsuario = document.getElementById("user");
+const inputDaSenha = document.getElementById("senha");
+const botaoEntrar = document.getElementById("btn-entrar");
+const mensagemLogin = document.getElementById("mensagem-login");
+
+function validarLogin() {
+    const usuarioDigitado = inputDoUsuario.value.trim();
+    const senhaDigitada = inputDaSenha.value.trim();
+
+    if (usuarioDigitado == usuario && senhaDigitada == senha) {
+        window.location.href = "home.html";
+        return;
+    }
+
+    mensagemLogin.textContent = "Login invalido";
+    mensagemLogin.classList.add("erro");
+}
+
+botaoEntrar.addEventListener("click", validarLogin);
+
+inputDaSenha.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        validarLogin();
+    }
+});
